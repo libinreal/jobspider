50c15aea1068f54d0c99eb792c1ba935fcb0f79f
c2002 <libinstar126@126.com>
2017-12-26 18:56:30 +0800

    c

--- lagou/lagou/settings.py
+++ lagou/lagou/settings.py
@@ -27,15 +27,16 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 10
+DOWNLOAD_DELAY = 6
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 5
 #CONCURRENT_REQUESTS_PER_IP = 16
-CONCURRENT_ITEMS = 16
-CONCURRENT_REQUESTS = 16
+CONCURRENT_ITEMS = 1
+CONCURRENT_REQUESTS = 1
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
+COOKIES_DEBUG = True
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
